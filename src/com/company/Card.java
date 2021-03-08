bb953c90b5a36997cb5b82a5f7e83c93fe68aa78
allexandessokollov <allexandersokollov@gmail.com>
2021-03-08 13:55:43 +0300

    JAF

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,2 +1,26 @@
-package com.company;public class Card {
+package com.company;
+
+public class Card
+{
+    private final int  QUESTION_SIZE = 256;
+    private final int ANSWER_SIZE = 512;
+
+    private char[] question = new char[QUESTION_SIZE];
+    private char[] answer = new char[ANSWER_SIZE];
+
+    public Card(String question, String answer)
+    {
+        this.question = question.toCharArray();
+        this.answer = answer.toCharArray();
+    }
+
+    public void setAnswer(String answer)
+    {
+        this.answer = answer.toCharArray();
+    }
+
+    public void setQuestion(String question)
+    {
+        this.question = question.toCharArray();
+    }
 }
